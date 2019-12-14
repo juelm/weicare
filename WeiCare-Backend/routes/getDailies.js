6b0700a7ee022125e07a573becc323a46fd8ce2a
Matthew Juel <matthewjuel@Matthews-MacBook-Pro.local>
2019-12-13 19:25:18 -0800

    Added db call to viewDaily

--- WeiCare-Backend/routes/getDailies.js
+++ WeiCare-Backend/routes/getDailies.js
@@ -17,10 +17,34 @@ router.get('/all', function(req, res, next){
 	//res.send(result);
 });
 
+router.get('/:userName', function(req, res, next) {
+	let nameString = ""
+	//let selectString = `SELECT ParentID FROM PARENTS WHERE UserName = '${req.params.userName}'`;
+	let selectString = `SELECT Title, DailyText 
+						FROM DAYLIES JOIN CLASSES Using(ClassID) 
+						JOIN STUDENTS Using(ClassID) 
+						JOIN STUDENTPARENT Using(StudentID)
+						JOIN PARENTS Using(ParentID) 
+						WHERE UserName = '${req.params.userName}'`;
+
+	db.query(selectString, (error, result, fields) => {
+		if (error) {
+			console.log(error);
+			res.send(error);
+		}else{
+
+			console.log(result);
+            res.send(result);
+        }
+	});
+  
+	//res.send(json[req.params.userName]);
+  });
+
 module.exports = router;
 
 
-function selectColumns($PK, $dispDate, $dispName, $dispEmail, $numResults){
+function selectDailies($PK, $dispDate, $dispName, $dispEmail, $numResults){
 	$columnString = "SELECT";
 	$fromTable = "FROM MESSAGES";
 	$limit = "";
--- WeiCare-ReactApp/weicare/src/pages/parentPage/ParentViewDaily.js
+++ WeiCare-ReactApp/weicare/src/pages/parentPage/ParentViewDaily.js
@@ -1,42 +1,56 @@
 import React, { useEffect, useState } from "react";
 import { Card, Form, Input, Button } from '../../components/AuthForm';
+import getDailies from '../../modules/getDayliesMod.js';
 
 
 function ParentViewDaily() {
 
-    const[dailies, updateDailies] = useState();
+    const[daylies, setDaylies] = useState([]);
+    //const[user]
     
-    useEffect(() => {
-        // let requestOptions = {
-        //     mode: 'cors'
-        // }
+    // useEffect(() => {
+    //     let getDailies();
+    //     updateDailies(getDailies())},[]);
+    
+    // useEffect(() => {updateDailies(getDailies())},[]);
 
-        fetch('http://localhost:8080/api/getDailies/all')
-        .then(res => (res.json()))
-        .then(res => res.DailyText)
-        .then(res => updateDailies(res))
+    
+    async function fetchDaylies(){
+        let res = await fetch('http://localhost:8080/api/getDailies/brownm');
+        res.json()
+        .then(res => console.log(res))
+        .then(res => setDaylies(res))
         .catch(err => console.log(err))
+        console.log(daylies);
+    }
+
+    useEffect(() => {
+        fetchDaylies();
         
     });
 
     //console.log(dailies)
 
     return (
+        
         <div style={{ margin: '175px 40%' }}>
+            {console.log(typeof(daylies))}
+            {console.log("this is in return: " + daylies)}
+            {
+            daylies ?
+            <h3>{daylies[0]}</h3>
+            // daylies.map(daylies =>(
+                
+            //     <div>
+            //         <h3>{daylies[0].title}</h3>
+            //     </div>
+            // ))
+            :
+            <h3>loading</h3>
+            
+        }
+        {console.log(daylies)}
 
-            <h1>{dailies}</h1>
-            {/* <Card>
-                <Form>
-                    <Input
-                        type="day"
-                        placeholder="Day" />
-                    <Input
-                        type="class"
-                        placeholder="Class"
-                    />
-                    <Button>View Daily!</Button>
-                </Form>
-            </Card> */}
         </div>
     );
 }
