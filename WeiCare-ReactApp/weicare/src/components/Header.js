63b7864e4a9bfb6f0da1db3181c6169cbcc2c6a6
Wei Kuang <50929180+kuangwei8@users.noreply.github.com>
2019-12-16 10:28:01 -0800

    minor changes

--- WeiCare-ReactApp/weicare/src/components/Header.js
+++ WeiCare-ReactApp/weicare/src/components/Header.js
@@ -2,6 +2,7 @@ import React from "react";
 import {AppBar, Toolbar, Grid, Typography, Button} from "@material-ui/core";
 import { Link } from "react-router-dom";
 import { useAuth } from "../context/auth"
+import {linkStyle} from "../pages/styles";
 
 
 export default function Header() {
@@ -9,38 +10,36 @@ export default function Header() {
   const { authTokens } = useAuth();
 
   return (
- 
-      <AppBar position="static">
-        <Toolbar>
-          <Grid container justify="space-between" spacing={24}>
-            <Grid item>
-              <Typography variant="h4">WeiCare</Typography>
-            </Grid>
-            <Grid item>
-              <Link to="/">
-                <Button color="inherit">Home</Button>
-              </Link>
-              <Link to="/about">
-                <Button color="inherit">About us</Button>
-              </Link>
-              <Link to="/contact">
-                <Button color="inherit">Contact</Button>
-              </Link>
-                {console.log("authTokens in header : " + authTokens)}
-                {authTokens ? (
-                  <Link to="/logout">
-                    <Button color="inherit">Logout</Button>
-                  </Link>
-                ) : (
-                  <Link to="/login">
-                    <Button color="inherit">Login</Button>
-                  </Link>
-                )}
- 
-            </Grid>
+    <AppBar position="static" >
+      <Toolbar>
+        <Grid container justify="space-between" spacing={24}>
+          <Grid item>
+            <Typography variant="h4">WeiCare</Typography>
           </Grid>
-        </Toolbar>
-      </AppBar>
+          <Grid item>
+            <Link to="/" style={linkStyle()}>
+              <Button color="inherit">Home</Button>
+            </Link>
+            <Link to="/about" style={linkStyle()}>
+              <Button color="inherit">About us</Button>
+            </Link>
+            <Link to="/contact" style={linkStyle()}>
+              <Button color="inherit">Contact</Button>
+            </Link>
+            {console.log("authTokens in header : " + authTokens)}
+            {authTokens ? (
+              <Link to="/logout" style={linkStyle()}>
+                <Button color="inherit">Logout</Button>
+              </Link>
+            ) : (
+              <Link to="/login" style={linkStyle()}>
+                <Button color="inherit">Login</Button>
+              </Link>
+            )}
+          </Grid>
+        </Grid>
+      </Toolbar>
+    </AppBar>
   );
 }
 
