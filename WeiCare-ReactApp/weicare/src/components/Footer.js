1a90e2b11e693cde73f381ae134201884ecfd959
mgorzelsky <m.d.gorzelsky@gmail.com>
2019-12-17 13:58:36 -0800

    All errors and warnings removed

--- WeiCare-ReactApp/weicare/src/components/Footer.js
+++ WeiCare-ReactApp/weicare/src/components/Footer.js
@@ -2,21 +2,23 @@ import React from "react";
 import { Tabs, Tab, Paper } from "@material-ui/core";
 
 function Footer() {
+  const selectedTab = "Selection";
   return (
     <Paper style={{clear:'both'}}>
       <Tabs
+        value={selectedTab}
         indicatorColor="primary"
         textColor="primary"
         variant="fullWidth"
         centered
       >
-        <Tab label=" &copy; 2019 Copyright: WeiCare " />
-        <Tab
+        <Tab value={selectedTab} label=" &copy; 2019 Copyright: WeiCare " />
+        <Tab value={selectedTab}
           label='Little school of big discoveries 
                 2140 Lucien-Thimens, St Laurent, H4R 1L1 
                 Tel: 514-303-8949'
         />
-        <Tab
+        <Tab value={selectedTab}
           label="
                 Open hours:
                 Monday to Friday 7:00-18:00
