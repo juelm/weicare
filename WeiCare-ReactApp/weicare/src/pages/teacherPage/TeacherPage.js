72ec9299e053b7d10c1cd3ed0db8a4649da4babd
Matthew Juel <matthewjuel@Matthews-MacBook-Pro.local>
2019-12-17 20:37:07 -0800

    Stash Changes

--- WeiCare-ReactApp/weicare/src/pages/teacherPage/TeacherPage.js
+++ WeiCare-ReactApp/weicare/src/pages/teacherPage/TeacherPage.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import PhotoPane from '../../../possible_garbage/PhontoPane'
 
 function TeacherPage() {
   return (
     <div>
-      
+      <PhotoPane />
     </div>
   )
 }
